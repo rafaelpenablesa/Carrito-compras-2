ea4a3753ea27b6201d18b24deafebfd386d04f8f
rafaelpenablesa <rafaelpenablesa@gmail.com>
2022-08-28 09:59:31 -0500

    Primer draft searchbar

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,26 @@
+
+const search = () =>{
+    const searchbox = document.getElementById("search-item").value.toUpperCase();
+    const storeitems = document.getElementById("pills-tabContent")
+    const product = document.querySelectorAll(".card-body")
+    const pname = document.getElementsByTagName("h5")
+
+    for (var i=0; i < pname.length; i++){
+        let match = product[i].getElementsByTagName('h5')[0];
+        if (match){
+            let textvalue = match.textContent || match.innerHTML
+            if(textvalue.toUpperCase().indexOf(searchbox) > -1){
+                product[i].style.display = "";
+            }else{
+                product[i].style.display = "none";
+            }
+        } 
+    }
+}
+
+
+
+
 const clickButton = document.querySelectorAll(".button");
 const tbody = document.querySelector(".tbody");
 let carrito = []; //* Arreglo carrito 
